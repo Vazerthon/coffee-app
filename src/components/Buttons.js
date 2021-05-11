03921b79e8a22fd74670929c47053ca11bec2ad9
Paul Vallis <vallis1@gmail.com>
2021-05-11 14:45:54 +0100

    Refactor edit button to icon button

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled/macro';
 import PropTypes from 'prop-types';
-import { Edit, Plus } from './Icons';
+import { Plus } from './Icons';
 
 const RoundButtonBase = styled.button`
   border: 1px solid ${({ theme }) => theme.colour.primary};
@@ -46,28 +46,33 @@ export function AddButton({ className, onClick }) {
   );
 }
 
-export function EditButton({ onClick, className }) {
-  return (
-    <RoundButtonBase small onClick={onClick} className={className}>
-      <Edit role="img" aria-label="Edit" />
-    </RoundButtonBase>
-  );
-}
-
-const propTypes = {
+AddButton.propTypes = {
   className: PropTypes.string,
   onClick: PropTypes.func.isRequired,
 };
 
-const defaultProps = {
+AddButton.defaultProps = {
   className: undefined,
 };
 
-AddButton.propTypes = propTypes;
-AddButton.defaultProps = defaultProps;
+export function IconButton({ onClick, className, icon: Icon, ariaLabel }) {
+  return (
+    <RoundButtonBase small onClick={onClick} className={className}>
+      <Icon role="img" aria-label={ariaLabel} />
+    </RoundButtonBase>
+  );
+}
 
-EditButton.propTypes = propTypes;
-EditButton.defaultProps = defaultProps;
+IconButton.propTypes = {
+  className: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  icon: PropTypes.func.isRequired,
+  ariaLabel: PropTypes.string.isRequired,
+};
+
+IconButton.defaultProps = {
+  className: undefined,
+};
 
 export const Button = styled.button`
   height: ${({ theme }) => theme.spacing.units(8)};
