212b6be0151d83349aca49e1b6a1be5db124177d
Paul Vallis <vallis1@gmail.com>
2021-05-16 18:47:38 +0100

    Add stats to bean card

--- src/components/BeanCard/BeanCard.js
+++ src/components/BeanCard/BeanCard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { H2 } from '../Typography';
 import { Beans } from '../Icons';
+import { brewType } from '../../Types';
 
 const Container = styled.div`
   border: 1px solid ${({ theme }) => theme.colour.tertiary};
@@ -23,21 +24,40 @@ const Title = styled(H2)`
   }
 `;
 
+const List = styled.ul`
+  margin: ${({ theme }) => theme.spacing.units(2)} 0;
+`;
+
 const Textarea = styled.textarea`
   width: calc(100% - ${({ theme }) => theme.spacing.units(5)});
   padding: ${({ theme }) => theme.spacing.units(2)};
-  margin-top: ${({ theme }) => theme.spacing.units(2)};
   min-height: ${({ theme }) => theme.spacing.units(20)};
   color: ${({ theme }) => theme.colour.primary};
 `;
 
-export default function BeanCard({ className, bean, note, onChange }) {
+export default function BeanCard({
+  className,
+  bean,
+  note,
+  allBrews,
+  onChange,
+}) {
   const handleChange = ({ target }) => onChange(bean, target.value);
+  const brewsOfThisBean = allBrews.filter((brew) => brew.bean === bean);
+  const brewCount = brewsOfThisBean.length;
+  const totalWeight = brewsOfThisBean
+    .map(({ groundsWeight }) => groundsWeight)
+    .reduce((p, c) => p + c, 0);
+
   return (
     <Container className={className}>
       <Title centre capitalise>
         <Beans /> {bean}
       </Title>
+      <List>
+        <li>Brews: {brewCount}</li>
+        <li>Weight: {totalWeight}g</li>
+      </List>
       <Textarea
         value={note}
         placeholder={`Add some notes about ${bean}`}
@@ -52,6 +72,7 @@ BeanCard.propTypes = {
   onChange: PropTypes.func.isRequired,
   note: PropTypes.string,
   className: PropTypes.string,
+  allBrews: PropTypes.arrayOf(brewType).isRequired,
 };
 
 BeanCard.defaultProps = {
